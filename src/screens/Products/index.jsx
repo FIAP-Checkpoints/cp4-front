263cd5093ffdb6474c252813645ff5ab39dd2ed9
imneli <contatodoneli@gmail.com>
2024-09-02 13:46:06 -0300

    Assets & Other

--- src/screens/Products/index.jsx
+++ src/screens/Products/index.jsx
@@ -10,6 +10,7 @@ function Products() {
         { id: 6, name: "Dumbbell Set", price: 79.99, image: "https://images.unsplash.com/photo-1519861531473-9200262188bf?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
         { id: 7, name: "Swimming Goggles", price: 15.99, image: "https://images.unsplash.com/photo-1519861531473-9200262188bf?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
         { id: 8, name: "Cycling Helmet", price: 49.99, image: "https://images.unsplash.com/photo-1519861531473-9200262188bf?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+        { id: 9, name: "Cycling Helmet", price: 49.99, image: "https://images.unsplash.com/photo-1519861531473-9200262188bf?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" }
     ];
 
     return (
